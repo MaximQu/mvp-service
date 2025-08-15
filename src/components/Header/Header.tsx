add413a3ac8da706f74806299878fa6ed8b966ad
MaximQu <makasyslar@gmail.com>
2025-08-15 15:17:07 +0300

    Fix logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -30,7 +30,7 @@ const Header = () => {
     <header className="py-2 bg-blue-900 sticky top-0">
       <div className="container mx-auto flex items-center justify-center gap-8 ">
         <a className="w-32" href="#">
-          <img src="/logo.jpg"  alt="" />
+          <img src="/public/logo.jpg" alt="AppliFix" />
         </a>
         <nav className="">
           <ul className="flex gap-8">
